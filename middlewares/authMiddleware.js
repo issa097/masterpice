7676f14e7cdd7a35bcb0630ca0209c9b1a464ea0
issa097 <haddadissa962@gmail.com>
2023-11-28 21:22:00 +0300

    add new authmiddleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -69,3 +69,51 @@ const authenticateToken = async (req, res, next) => {
 };
 
 module.exports = { authenticateToken };
+
+
+
+// const jwt = require("jsonwebtoken");
+// const User = require("../models/users");
+// // require('dotenv').config();
+// const SECRET_KEY = "issa";
+
+// const authenticateToken = async (req, res, next) => {
+//   try {
+//     const token = req.headers.authorization;
+//     console.log("😜😜😜😜", token);
+//     if (!token) {
+//       return res
+//         .status(401)
+//         .json({ success: false, message: "You need to login first" });
+//     }
+
+//     const decoded = jwt.verify(token, SECRET_KEY);
+
+//     if (!decoded.user_id) {
+//       return res
+//         .status(401)
+//         .json({ success: false, message: "Unauthorized: User not found" });
+//     }
+
+//     // Optionally, you can check if the user exists in your database
+//     // const user = await User.findById(decoded.user_id);
+//     // if (!user) {
+//     //   return res
+//     //     .status(401)
+//     //     .json({
+//     //       success: false,
+//     //       message: "Unauthorized: User not found in the database",
+//     //     });
+//     // }
+
+//     req.user = decoded.user_id; // Set the user in the request object for later use
+//     next();
+//   } catch (error) {
+//     console.error(error);
+//     return res
+//       .status(401)
+//       .json({ success: false, message: "Unauthorized: Invalid token" });
+//   }
+// };
+
+// module.exports = { authenticateToken };
